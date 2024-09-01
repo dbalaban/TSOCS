345460441e8919bb4dd6d0868aa9af973a7020b2
Ubuntu <ubuntu@ip-172-30-2-35.ec2.internal>
2024-09-01 14:34:52 +0000

    new output format

--- src/lib/core/src/TestMain.cpp
+++ src/lib/core/src/TestMain.cpp
@@ -13,34 +13,54 @@
 #include <stdio.h>
 #include <iostream>
 
-const int ITERATIONS = 10000;
+const long ITERATIONS = 100;
 
 int main(int argc, char* argv[])
 {
-    long bsuccesses = 0, asuccesses = 0;
-    long bfailures = 0,  afailures = 0;
+
+    long iterations = 0;
+    long solvedonlya = 0, solvedonlyb = 0, solvedboth = 0;
+    long failedonla = 0, failedonlyb, failedboth = 0;
+
+    if (argc > 1)
+        iterations = atol(argv[1]);
+    else iterations = ITERATIONS;
 
     RandomProblemGenerator pgen(3,7);
 
-    for (long i = 0; i < ITERATIONS; i++)
+    for (long i = 0; i < iterations; i++)
     {
         TOCORBA problem = pgen.generateProblem();
         ProblemSolver solver(problem);
 
         AngleFormulation aform;
-        bool ok = solver.Solve((AbstractFormulation<AngleFormulation>*)(&aform), ProblemType::TSOCStype);
-        if (ok)
-            asuccesses++;
-        else afailures++;
+        bool oka = solver.Solve((AbstractFormulation<AngleFormulation>*)(&aform), ProblemType::TSOCStype);
 
         BasicFormulation bform;
-        ok = solver.Solve((AbstractFormulation<BasicFormulation>*)(&bform), ProblemType::TSOCStype);
-        if (ok)
-            bsuccesses++;
-        else bfailures++;
+        bool okb = solver.Solve((AbstractFormulation<BasicFormulation>*)(&bform), ProblemType::TSOCStype);
+
+        if (oka && okb)
+            solvedboth++;
+        else if (!oka && !okb)
+            failedboth++;
+        else if (oka)
+        {
+            solvedonlya++;
+            failedonlyb++;
+        }
+        else if (okb)
+        {
+            solvedonlyb++;
+            failedonla++;
+        }
     }   
 
-    std::cout << std::endl << "Results of " << ITERATIONS << " iterations (BasicFormulations): " << bsuccesses << " success, " << bfailures << " failed" << std::endl;
-    std::cout << "Results of " << ITERATIONS << " iterations (AngleFormulations): " << asuccesses << " success, " << afailures << " failed" << std::endl;
-    
+    // report results in tabular format
+    std::cout << std::endl << "Results from " << iterations << " iterations:" << std::endl;
+    std::cout << "\t\tSuccess\t\tFailure" << std::endl 
+              << "\t\t-------\t\t-------" << std::endl;
+    std::cout << "\tBasic:\t" << solvedonlyb << "\t\t" << failedonlyb << std::endl;
+    std::cout << "\tAngle:\t" << solvedonlya << "\t\t" << failedonla  << std::endl;
+    std::cout << "\tBoth:\t"  << solvedboth  << "\t\t" << failedboth  << std::endl << std::endl;
+
 }
